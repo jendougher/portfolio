ac83c32def929491bf5eb2347e29e2097c3a10e6
Jennifer Dougher <jenniferdougher@Jennifers-MacBook-Pro.local>
2018-10-25 23:48:27 -0400

    removed resume button, added button animations and jquery scrolling effects on nav links.

---
+++ js/portfolio.js
@@ -0,0 +1,36 @@
+
+$(document).ready(function() {
+    
+  
+$('.js--scroll-to-about').click(function () {
+   $('html, body').animate({scrollTop: $('.js--section-about').offset().top}, 1000);
+     });  
+    
+$('.js--scroll-to-tools').click(function () {
+   $('html, body').animate({scrollTop: $('.js--section-tools').offset().top}, 1000);
+     });  
+    
+$('.js--scroll-to-projects').click(function () {
+   $('html, body').animate({scrollTop: $('.js--section-projects').offset().top}, 1000);
+     });  
+    
+$('.js--scroll-to-form').click(function () {
+   $('html, body').animate({scrollTop: $('.js--section-form').offset().top}, 1000);
+     });
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+});
+
+    
+
+
